27a25b5eb501832f851e1f287c6d829016f89463
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-10-03 17:38:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/malik7983/file-filter-bot.git /file-filter-bot
+  git clone https://github.com/Rajneesh058/movie-finder-bot058 /movie-finder-bot058
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /file-filter-bot
+  git clone $UPSTREAM_REPO /movie-finder-bot058
 fi
-cd /file-filter-bot
+cd /movie-finder-bot058
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
