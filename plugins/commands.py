0cf92292cfee82036a3752e863b4dae5dd43426a
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-11-06 17:27:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -267,7 +267,7 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('♻️ 𝐉𝐨𝐢𝐧 𝙂𝙧𝙤𝙪𝙥 ', url="https://t.me/Hindi_Movie_Group_1")
+                    InlineKeyboardButton('♻️ 𝐉𝐨𝐢𝐧 𝙂𝙧𝙤𝙪𝙥 ', url="https://t.me/movie_request_group_058")
                 ]
             ]
         )
