697131a4f2b66ae368edffdc3cbeb01b6a8bd7cb
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-11-15 14:58:07 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -248,8 +248,8 @@ _𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐌𝐞 𝐅𝐨𝐫 𝐊𝐧𝐨𝐰 𝐀𝐛�
 
 𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/Rajneesh_Singh_Tomar>Rajneesh_Singh_Tomar</a></b>"""
 
-    FORCESUB_TXT = """<b>
- 🔐 Join Our Update Channel To Use The Bot 🔐 </b>"""
+    FORCESUB_TXT = """
+ 🔐 Join Our Update Channel To Use The Bot 🔐 """
     
     OWNER_TXT = """<b>>━━━━᚜ Owner Details ᚛━━━━<
     
