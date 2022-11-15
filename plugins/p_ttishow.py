791f4990afec0fba77589467f01e08b7622d8358
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-11-16 00:27:59 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,9 +37,9 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('♻️ Help ♻️', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('💎 Updates 💎', url='https://t.me/+Dek49ihM4u5iNWQ1')
-        ],[InlineKeyboardButton('🌴 Bots Channel 🌴', url='https://t.me/Epic_creation_bots')]]
+            InlineKeyboardButton('ʜᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            InlineKeyboardButton('🎭ᴍᴏᴠɪᴇ ᴜᴘᴅᴀᴛᴇ🎭', url='https://t.me/+Dek49ihM4u5iNWQ1')
+        ],[InlineKeyboardButton(' ʙᴏᴛ ᴄʜᴀɴɴᴇʟ ', url='https://t.me/Epic_creation_bots')]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=(PHT),
@@ -59,7 +59,7 @@ async def save_group(bot, message):
                                                  text=(MALIK_PHH.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
-                                                                           InlineKeyboardButton('🎭MOVIE CHANNEL🎭',url='https://t.me/+Dek49ihM4u5iNWQ1')
+                                                                           InlineKeyboardButton('🎭ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ🎭',url='https://t.me/+Dek49ihM4u5iNWQ1')
                                                                           ]]
                                                  ),
                                                  parse_mode='html'
