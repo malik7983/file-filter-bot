577942f8888e265aa90e0b9821c1d10126ddd5ef
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-11-15 14:18:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,9 +49,10 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", {file_name}
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", 
+ {file_name}
 
-🔘 size - {file_size}
+
 
        ┏━━━━•❅•°•❈•°•❅•━━━━┓
 
