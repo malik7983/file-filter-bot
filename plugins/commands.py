bdc8817d6d3fe000e928f54812b4b1bb5b42df5c
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-11-16 21:36:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -58,41 +58,74 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('ꜱᴇᴀʀᴄʜ ɪɴʟɪɴᴇ', switch_inline_query_current_chat=''),
             InlineKeyboardButton('ᴜʀʟ ꜱʜᴏʀᴛɴᴇʀ', callback_data='urlshortn')
-            ]]
+             ]]
+
         reply_markup = InlineKeyboardMarkup(buttons)
+
         await message.reply_photo(
+
             photo=random.choice(PICS),
+
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+
             reply_markup=reply_markup,
+
             parse_mode='html'
+
         )
+
         return
+
     if AUTH_CHANNEL and not await is_subscribed(client, message):
+
         try:
+
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
+
         except ChatAdminRequired:
+
             logger.error("Make sure Bot is admin in Forcesub channel")
+
             return
+
         btn = [
+
             [
+
                 InlineKeyboardButton(
-                    "Jᴏɪɴ ɴᴏᴡ 🔓", url=invite_link.invite_link
+
+                    "ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=invite_link.invite_link
+
                 )
+
             ]
+
         ]
 
         if message.command[1] != "subscribe":
+
             kk, file_id = message.command[1].split("_", 1)
+
             pre = 'checksubp' if kk == 'filep' else 'checksub' 
-            btn.append([InlineKeyboardButton("Try Again", callback_data=f"{pre}#{file_id}")])
+
+            btn.append([InlineKeyboardButton("♻️ Try Again ♻️", callback_data=f"{pre}#{file_id}")])
+
         await client.send_message(
+
             chat_id=message.from_user.id,
+
             text=script.FORCESUB_TXT,
+
             reply_markup=InlineKeyboardMarkup(btn),
+
             parse_mode="markdown"
+
             )
+
         return
+
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
+
         buttons = [[
             InlineKeyboardButton('ᴀᴅᴅ ᴛᴏ ɢʀᴏᴜᴘ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
